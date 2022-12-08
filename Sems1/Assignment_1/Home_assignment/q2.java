621e63b3afde8fa7ee35226389e2d1ef87f84f13
Aditya Choudhury <adityachoudhury0230@gmail.com>
2022-12-08 13:49:35 +0530

    Synced local repo changes

---
+++ Sems1/Assignment_1/Home_assignment/q2.java
@@ -0,0 +1,14 @@
+package Sems1.Assignment_1.Home_assignment;
+/*
+ What do each of the following print?
+a) System.out.println('b');
+b) System.out.println('b' + 'c');
+c) System.out.println((char) ('a' + 4));
+ */
+public class q2 {
+    public static void main(String[]Args){
+        System.out.println('b'); //Output: b
+        System.out.println('b' + 'c'); //Output: 197
+        System.out.println((char) ('a' + 4)); //Output: e
+    }
+}
---
+++ Sems1/Assignment_1/Home_assignment/q3.java
@@ -0,0 +1,23 @@
+package Sems1.Assignment_1.Home_assignment;
+/*
+ Suppose that a variable a is declared as int a = 2147483647 (or equivalently,
+Integer.MAX_VALUE ). What do each of the following print?
+a) System.out.println(a);
+b) System.out.println(a+1);
+c) System.out.println(2-a);
+d) System.out.println(-2-a)
+e) System.out.println(2*a);
+f) System.out.println(4*a);
+ */
+public class q3 {
+    public static void main(String[]Args){
+        int a = 2147483647 ;
+        System.out.println(a); //Output: 2147483647
+        System.out.println(a+1); //Output: -2147483648
+        System.out.println(2-a); //Output: -2147483645
+        System.out.println(-2-a); //Output: 2147483647
+        System.out.println(2*a); //Output: -2
+        System.out.println(4*a); //Output: -4
+
+    }
+}
