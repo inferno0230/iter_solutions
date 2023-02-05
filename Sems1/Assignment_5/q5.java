42456fbde0f4d7c8d502982bd764459dce4c0948
inferno0230 <adityachoudhury0230@gmail.com>
2023-02-05 18:13:34 +0530

    Partially add Assignment_5

---
+++ Sems1/Assignment_5/q5.java
@@ -0,0 +1,15 @@
+package Sems1.Assignment_5;
+/*  Write a program to display the multiplication table from 2 to 15. */
+
+public class q5{
+    public static void main(String[]Args){
+        for(int i = 2; i <= 15; i++) {
+            System.out.println("Multiplication table of "+i);
+          for(int j = 1; j <=10; j++) {
+            System.out.println(i + " x " + j + " = " + i*j);
+          }
+          System.out.println();
+        }
+        
+    }
+}
