87470bc5e631ec7c7f2c482b1117110f138113d8
inferno0230 <adityachoudhury0230@gmail.com>
2023-02-05 22:04:42 +0530

    Sync Q7 series of Assignment 5

---
+++ Sems1/Assignment_5/q7a.java
@@ -0,0 +1,21 @@
+package Sems1.Assignment_5;
+/* Write a program to print the following outputs using for loops:
+ A
+ A B
+ A B C
+ A B C D
+ A B C D E
+ */
+public class q7a {
+    public static void main(String[]Args){
+        for (int i = 1; i <= 5; i++) {
+          for (int j = 'A'+1; j -'A' <= i; j++) {  // Use ASCII value of "A" in code
+            int k = 0;
+            k+=j;
+            System.out.print((char)((int)k-1)+" "); // Typecast value of k into char since value of k has added ASCII value of 65 in it.
+          }
+          System.out.println();
+        }
+        
+    }
+}
---
+++ Sems1/Assignment_5/q7c.java
@@ -0,0 +1,16 @@
+package Sems1.Assignment_5;
+
+public class q7c {
+    public static void main(String[]Args){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+        
+    }
+}
