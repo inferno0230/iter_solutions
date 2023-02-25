d49b1fa4b141689cf84cc07416d3788233157de1
Kamallochan Das <118735381+kamallochan2004@users.noreply.github.com>
2023-02-25 10:22:36 +0530

    Completed the solution

--- Sems1/Assignment_4/Home_assignment/q5.java
+++ Sems1/Assignment_4/Home_assignment/q5.java
@@ -11,15 +11,18 @@ import java.util.Scanner;
 
 public class q5 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+      Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         input.close();
-
-        for (int i=1;i<=n;i++){
-            /* PLACEHOLDER 
-            WILL DO LATER */
+        String p="";
+        for (int i=1;i<=n;i++) {
+            p+=(i+p);
+            System.out.println(p);
         }
+
     }
 }
-    
+
+
+
 
