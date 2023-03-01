8d765c3bbd068e4050fe17d18195c2a487617110
Kamallochan Das <118735381+kamallochan2004@users.noreply.github.com>
2023-03-01 20:13:06 +0530

    Update to P1Q1.java
    The Question asked to store random values between 1 and the array size and find their sum and avg.
    The code is now correctly updated according to the question given.

--- Sems1/Practice_questions/P1Q1.java
+++ Sems1/Practice_questions/P1Q1.java
@@ -1,33 +1,34 @@
 
-import java.util.Scanner;
+import java.util.*;
 public class P1Q1 {
         public static void main(String[]Args){
             Scanner input = new Scanner(System.in);
-            System.out.print("Enter value of N: ");
-            int size = input.nextInt();
-            int a[] = new int[size];
-            System.out.println("Enter "+size+" numbers: ");
-            for (int i=0;i<a.length;i++){
-            a[i] = input.nextInt();
-            }
-            input.close();
+            System.out.print("Enter size of array:");
+        int size=sc.nextInt();
+        int n[]=new int[size];
+        sc.close();
+        
+            for(int i=0;i<=n.length-1;i++) {
+            Random rand = new Random();
+            n[i] = rand.nextInt(1, size);
+        }
 
-            System.out.println(add(a));
-            System.out.println(avg(a));
+        System.out.println("The sum of value's present in array is "+add(n));
+        System.out.println("the avg of value's present in array is "+avg(n));
         }
 
         public static int add(int a[]){
-            int sum = 0;
-            for (int i=0;i<a.length;i++){
-                sum+=a[i];
-            }
-            return sum;
+        int sum = 0;
+        for (int i=0;i<a.length;i++){
+            sum+=a[i];
         }
-        
-        public static double avg(int a[]){
-            double avg = add(a)/a.length;
-            return avg;
-        }
+        return sum;
+    }
 
-        
+    public static double avg(int a[]){
+        double avg = add(a)/a.length;
+        return avg;
+    }
+    
 }
+
