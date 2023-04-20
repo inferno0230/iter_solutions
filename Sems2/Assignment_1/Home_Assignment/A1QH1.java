25591dd155e869bb34118211a88413c8545bb2fb
inferno0230 <adityachoudhury0230@gmail.com>
2023-04-20 11:44:21 +0530

    No more ass1

--- Sems2/Assignment_1/Home_Assignment/A1QH1.java
+++ Sems2/Assignment_1/Home_Assignment/A1QH1.java
@@ -3,6 +3,16 @@ package Sems2.Assignment_1.Home_Assignment;
 import java.util.Scanner;
 public class A1QH1 {
     public static void main(String[]args){
-        // The heck does the question even mean
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        input.close();
+        if(a+b==c && a == b - c || a*b==c){
+            System.out.println(a+","+","+"c"+" can be used in correct arithmetic formula");
+        } else {
+            System.out.println(a+","+","+"c"+" cannot be used in correct arithmetic formula");
+        }
     }
 }
--- Sems2/Assignment_1/Home_Assignment/A1QH2.java
+++ Sems2/Assignment_1/Home_Assignment/A1QH2.java
@@ -1,5 +1,22 @@
 package Sems2.Assignment_1.Home_Assignment;
 
+import java.util.Scanner;
 public class A1QH2 {
-    // The heck does the question even mean
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Emter three lines: ");
+        String x[] = new String[3];
+        x[0]=input.nextLine();
+        x[1]=input.nextLine();
+        x[2]=input.nextLine();
+        input.close();
+        System.out.println("\nReversed order: \n");
+        for(int i=x.length-1;i>=0;i--){
+            System.out.println(x[i]+"");
+        }
+
+
+    }
+
 }
+
